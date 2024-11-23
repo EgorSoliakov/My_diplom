e4f31fe96528a03c778e693be80256766dca88bc
Egor.S <Egorsoliakov@gmail.com>
2024-11-23 13:24:23 +0400

    new

--- auth-app/src/App.js
+++ auth-app/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg'; 
+import React from 'react';
 import './App.css'; 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SignIn from './Screens/Signin';
